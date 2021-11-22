7c26e1fe0b6725fbf25494fb40287be0c21e2a07
hoodjustin <82976113+hoodjustin@users.noreply.github.com>
2021-11-21 16:04:13 -0800

    added time table

--- peer-up-x/src/Profile/SupporterProfile.js
+++ peer-up-x/src/Profile/SupporterProfile.js
@@ -1,6 +1,8 @@
 import styles from './SupporterProfile.module.css';
 import SideBar from '../SideBar';
 import SupporterEditor from './SupporterEditor';
+import './SupporterProfile.css'
+
 export default function SupporterProfile(props){
 
   
@@ -9,8 +11,30 @@ export default function SupporterProfile(props){
       <SideBar profile = {true} dashboard = {false}/>
       <div className = {styles.rightContentContainer}>
         <SupporterEditor/>
-      </div>
     </div>
-
+    <div className="time-table">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">Monday</th>
+          <th scope="col">Wednesday</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">8-9 PM</th>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">9-10 PM</th>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+    </div>
   );
 }
