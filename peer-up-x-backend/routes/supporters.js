6fe1682350a17741ed426b6fe52a2fdc9d924cca
Aman Oberoi <amanbir5103@gmail.com>
2022-02-06 23:05:43 -0800

    fixed /register endpoint issue - endpoint now saves the hashed pwd in db

--- peer-up-x-backend/routes/supporters.js
+++ peer-up-x-backend/routes/supporters.js
@@ -6,120 +6,84 @@ const router = express.Router();
 
 const hashPassword = async (password, saltRounds = 10) => {
   try {
-      // Generate a salt
-      const salt = await bcrypt.genSalt(saltRounds);
+    // Generate a salt
+    const salt = await bcrypt.genSalt(saltRounds);
 
-      // Hash password
-      return await bcrypt.hash(password, salt);
+    // Hash password
+    return await bcrypt.hash(password, salt);
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 
   // Return null if error
   return null;
 };
 
-router.post("/register", async(req, res) => {
-    Supporter.findOne({
-        $or:
-            [{"email":req.body.email}]
-    }).then(supporter => {
-        if (supporter) {             
-            return res.status(202).send({
-                message: "This user already exists!"
-            })
-        }
-        var hashedPassword;
-        // generate salt to hash password
-        const salt = await bcrypt.genSalt(10);
-      // now we set user password to hashed password
-        hashedPassword = await bcrypt.hash(user.password, salt);
-        console.log("hashed password")
-        console.log(hashedPassword);
-        // hashPassword(req.body.password).then((x) => {
-        //   console.log("hello"); console.log(x); hashedPassword = x
-        // });
-        // console.log("hashed password");
-        // console.log(hashedPassword);
-      
-        // if(hashedPassword == null){
-        //   console.log("error hashing password");
-        // }
+router.post("/register", async (req, res) => {
 
-        const newSupporter = new Supporter({
-          nickname: req.body.nickname,
-          isAvailable: false, //assuming we want the user to sign in after registration
-          availabilityHours: req.body.availabilityHours, 
-          story : req.body.story,
-          school : req.body.school,
-          specialty: req.body.specialty,
-          isChatting: false,
-          email: req.body.email,
-          password: hashedPassword
-      }) //making sure our user input is in the right format
-        res.json(404, null);
-        
+  // generating a hashed version of the password
+  const hashedPassword = await hashPassword(req.body.password); 
 
-        // const newSupporter = new Supporter({
-        //     nickname: req.body.nickname,
-        //     isAvailable: false, //assuming we want the user to sign in after registration
-        //     availabilityHours: req.body.availabilityHours, 
-        //     story : req.body.story,
-        //     school : req.body.school,
-        //     specialty: req.body.specialty,
-        //     isChatting: false,
-        //     email: req.body.email,
-        //     password: hashedPassword
-        // }) //making sure our user input is in the right format
+  // creating the supporter object
+  const newSupporter = new Supporter({
+    nickname: req.body.nickname,
+    isAvailable: false, // assuming we want the user to sign in after registration
+    availabilityHours: req.body.availabilityHours,
+    story: req.body.story,
+    school: req.body.school,
+    specialty: req.body.specialty,
+    isChatting: false,
+    email: req.body.email,
+    password: hashedPassword
+  }) 
 
-        // newSupporter.save()
-        //     .catch(err => {
-        //         return res.status(500).json({
-        //             message: "Internal server error, please try again later!",
-        //             error: err
-        //         })
-        //     });
-
-        // res.json(201, newSupporter);
-      
-    })
-
-});
-
-router.get('/fetch/:email', function(req, res) {
-    Supporter.findOne({ // search by an email -> mostly so that we dont get multiple accounts with the same user
-      $or:
-          [{"email": req.params.email}]
-    }).then(supporter => {
-      if (!supporter) {
-        return res.status(200).send({
-          message: "Supporter not found"
-        })
-      }
-      return res.status(200).send({supporter, message: "Supporter found!"});   
-    }).catch(err => {
-      return res.status(500).send({
+  // saving the supporter in the database
+  newSupporter.save()
+    .catch(err => {
+      return res.status(500).json({
         message: "Internal server error, please try again later!",
         error: err
       })
+    });
+
+  res.json(201, newSupporter);
+
+});
+
+router.get('/fetch/:email', function (req, res) {
+  Supporter.findOne({ // search by an email -> mostly so that we dont get multiple accounts with the same user
+    $or:
+      [{ "email": req.params.email }]
+  }).then(supporter => {
+    if (!supporter) {
+      return res.status(200).send({
+        message: "Supporter not found"
+      })
+    }
+    return res.status(200).send({ supporter, message: "Supporter found!" });
+  }).catch(err => {
+    return res.status(500).send({
+      message: "Internal server error, please try again later!",
+      error: err
     })
+  })
 });
 
 //still needs testing; but could be connected to edit profile potentially
-router.put('/update/:supporterID', function(req, res) {
+router.put('/update/:supporterID', function (req, res) {
   Supporter.findOneAndUpdate({ // search by an id
-      $or:
-          [{"_id": req.params.supporterID}]      
+    $or:
+      [{ "_id": req.params.supporterID }]
   }, {
-        nickname: req.body.nickname,
-        //isAvailable: false, //assuming we want the user to sign in after registration
-        availabilityHours: req.body.availabilityHours, //!
-        story : req.body.story,
-        school : req.body.school,
-        specialty: req.body.specialty,
-        //isChatting: false,
-        email: req.body.email
-      }).then(supporter => {
+    nickname: req.body.nickname,
+    //isAvailable: false, //assuming we want the user to sign in after registration
+    availabilityHours: req.body.availabilityHours, //!
+    story: req.body.story,
+    school: req.body.school,
+    specialty: req.body.specialty,
+    //isChatting: false,
+    email: req.body.email
+  }).then(supporter => {
     console.log(supporter);
     if (!supporter) {
       return res.status(400).send({
@@ -128,24 +92,24 @@ router.put('/update/:supporterID', function(req, res) {
     }
     return res.status(200).send(supporter);
   }).catch(err => {
-      return res.status(500).send({
-          message: "Internal server error, please try again later!",
-          error: err
-      })
+    return res.status(500).send({
+      message: "Internal server error, please try again later!",
+      error: err
+    })
   });
 });
 
 //implement the fetchAll endpoint:
-router.get('/fetchAll', function (req, res){
+router.get('/fetchAll', function (req, res) {
   Supporter.find().then(supporters => {
-    if (!supporters){
+    if (!supporters) {
       //return an error
-      return res.status(202).send({ 
+      return res.status(202).send({
         message: 'did not find any'
       })
     }
-    else{
-      return res.status(200).send({supporters, message: "successful"})
+    else {
+      return res.status(200).send({ supporters, message: "successful" })
     }
   })
 })
